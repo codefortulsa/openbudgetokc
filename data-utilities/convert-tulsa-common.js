9ff95c100188dfac82876df59c89084f3375b109
LeaderOne <markelba1@gmail.com>
2017-01-22 13:03:00 -0600

    Operating budget parsed successfully... still need to get it into the format used by the code...

--- data-utilities/convert-tulsa-common.js
+++ data-utilities/convert-tulsa-common.js
@@ -32,7 +32,7 @@ characterClasses[55] = 'Debt Service';
 characterClasses[59] = 'Transfers';
 
 /**
- * Get the fund's 'category,' listed in Appendix 9 of the city's Budget Book.  This is not useful (yet).
+ * Get the fund's 'category,' listed in Appendix 9 of the city's Budget Book.
  * @param fundNumber The fund number
  * @returns {*} That funds category
  */
@@ -77,12 +77,19 @@ function getFundDescriptions(fundDescWkbk) {
     // (probably because they're capital projects, which are not part of the operating budget... i.e, it's 'capital',
     // not 'expense')
     //See https://www.cityoftulsa.org/media/570090/Section%203%20Funds.pdf and find the codes.
+    fundDescriptions[3703] = 'Tulsa Airports Improvement Trust Fund'; //https://www.cityoftulsa.org/media/233551/1A-PreIntroPages.pdf
+    fundDescriptions[6001] = 'Miscellaneous Capital Projects Fund';
+    fundDescriptions[6007] = '1996 Five-Year Sales Tax Fund'; //https://www.cityoftulsa.org/media/19330/section00cover&sectioncontents.pdf
+    fundDescriptions[6008] = '2001 Five-year Sales Tax Fund';
+    fundDescriptions[6009] = '2006 Special Extended Sales Tax Fund';
     fundDescriptions[6011] = '2008 Sales Tax Special Temporary Streets Fund';
     fundDescriptions[6012] = '1985 Sales Tax Economic Development Fund';
     fundDescriptions[6014] = '2014 Sales Tax Fund';
     fundDescriptions[6021] = 'TMUA-Water Capital Projects Fund';
     fundDescriptions[6031] = 'TMUA-Sewer Captial Projects Fund';
     fundDescriptions[6041] = 'Stormwater Capital Projects Fund';
+    fundDescriptions[6420] = 'E911 Fee Capital Fund'; //http://cityoftulsa.org/COTlegacy/documents/0708budget/Sec00-CoverAndSectionContents.pdf
+    fundDescriptions[6951] = 'RMUA - Capital Projects';
 
     if (!fundDescWkbk) {
         fundDescWkbk = xlsx.readFile(FUND_DESC_LOCATION);
--- data-utilities/convert-tulsa-opercap-excel.js
+++ data-utilities/convert-tulsa-opercap-excel.js
@@ -3,6 +3,8 @@ var jsonfile = require('jsonfile');
 var colIndex = require('./convert-tulsa-common').colIndex;
 var getFundNumbers = require('./convert-tulsa-common').getFundNumbers;
 var getFundDescriptions = require('./convert-tulsa-common').getFundDescriptions;
+var getOperUnitDesc = require('./convert-tulsa-common').getFundCategory;
+var characterClasses = require('./convert-tulsa-common').characterClasses;
 
 //Spreadsheet data
 var INPUT_SPREADSHEET_LOCATION = '../_src/data/tulsa/originals/FY17 Oper + Capital.xlsx';
@@ -12,19 +14,19 @@ var CATG_SHEET_NM = 'OPBUDProgDP (2)';
 
 //Fund code->Fund
 //Fund Description->Sheet 1
-//Operating unit->(code)
-//Operating Unit Description->Based on account code
+//Operating unit->(useless code)
+//Operating Unit Description->Based on fund code, get as you go
 //Agency->(pre) Dept
 //Agency Name->(post) Dept
-//Program ID-> (code)
+//Program ID-> (useless code)
 //Program Name-> Sheet 2 headings
-//LOB ID-> (code)
-//LOB Name->Based on appendix 9
+//LOB ID-> (useless code)
+//LOB Name->Based on appendix 9, get as you go
 //Account-> (code? - maybe [pre] Col A)
 //Character Class->Group code
 //Character Class Description->Cross-ref OKC
 //Account Description->Col A of Table (cross-ref sheet 2)
-//Operating or Non->Based on category code
+//Operating or Non->Based on fund code (6000 vs everything else), get as you go
 //FY2107 budget->Table
 
 
@@ -37,9 +39,60 @@ var COL_FOR_PGM_DESC = 'AK';
 var FIRST_ROW_PGM = 12;
 var FINAL_ROW_PGM = 57;
 
-var FINAL_FUND_COL = 41;
-var TOTAL_FUND_COL = 'AO';
+var FINAL_FUND_COL = 40;
 var FIRST_FUND_COL = 2;
+
+var ACCOUNT_DESCRIPTION_COL = 'A';
+var ACCOUNT_GROUP_CODE_COL = 'B';
+var FIRST_ROW_FOR_EXP = 3;
+var FINAL_ROW_FOR_EXP = 148;
+
+//From Appendix 9
+var AGENCY_LINE_OF_BUSINESS = [];
+AGENCY_LINE_OF_BUSINESS['Finance'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Human Resources'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Communications'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Municipal Court'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Information Technology'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Asset Management'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Customer Care'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Water & Sewer'] = 'Administration';
+
+AGENCY_LINE_OF_BUSINESS['Parks and Recreation'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Performing Arts Center'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['BOK and Convention Centers'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Planning and Development'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Streets and Stormwater'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Working in Neighborhoods'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Gilcrease'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['River Parks'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Engineering Services'] = 'Community Development and Transportation';
+
+//TODO: Ask Nathan about these!
+AGENCY_LINE_OF_BUSINESS['Debt Service'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Asset Management'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Transfers - Internal & Outside'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Legal'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Mayor\'s Office of Human Rights'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['City Council'] = 'City Council';
+AGENCY_LINE_OF_BUSINESS['City Auditor'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['Mayor'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['Tulsa Transit'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Water and Sewer'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Social and Economic Development'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Mayor\'s Office of Economic'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['River Parks Authority'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Gilcrease Museum'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Park and Recreation'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Tulsa Area Emergency Mgmt.'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['Emergency Medical Services Authority'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['Fire'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['Police'] = 'Mayor';
+AGENCY_LINE_OF_BUSINESS['General Government'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Employees Insurance Administration'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Worker\'s Compensation'] = 'Administration';
+AGENCY_LINE_OF_BUSINESS['Planning & Development'] = 'Community Development and Transportation';
+AGENCY_LINE_OF_BUSINESS['Mayor\'s Office of Economic Development'] = 'Community Development and Transportation';
 
 /**
  * Gets the revenue's 'Program' as listed on the second worksheet.
@@ -57,7 +110,7 @@ function getPrograms(TulsaSummOpCapWksht) {
 
         if(codedDescription) {
             var descriptionCode = codedDescription.v.substr(0,2);
-            descriptionsToPgm[codedDescription.v] = {program: program, description: prettyDesc, code: descriptionCode};
+            descriptionsToPgm[codedDescription.v] = {program: program, accountDescription: prettyDesc.v, accountCode: descriptionCode};
             console.log('Cell ' + COL_FOR_PGM_DESC + row + ': Program ' + program + ' has desc ' + codedDescription.v);
         } else if(!codedDescription && maybePgm) {
             program = maybePgm.v;
@@ -70,6 +123,72 @@ function getPrograms(TulsaSummOpCapWksht) {
     return descriptionsToPgm;
 }
 
+function getOperatingUnitType(accountCode) {
+    return accountCode > 6000 && accountCode < 7000 ? 'Capital' : 'Operating';
+}
+
+
+function getExpenseAmounts(ExpenseWksht, programs, fundNumbers, fundDescriptions) {
+    var departmentName = '****PLACEHOLDER****';
+    var programName = '****PLACEHOLDER****';
+    var expenses = [];
+    var i = 0;
+
+    for(var row=FIRST_ROW_FOR_EXP; row<FINAL_ROW_FOR_EXP; row++) {
+        var groupNumber = ExpenseWksht[ACCOUNT_GROUP_CODE_COL + row];
+        console.log('Row %d', row);
+
+        if(groupNumber) {
+            var departmentNameMaybe = ExpenseWksht[ACCOUNT_DESCRIPTION_COL + row];
+
+            if (departmentNameMaybe) {
+                departmentName = programs[departmentNameMaybe.v].accountDescription.trim();
+                programName = programs[departmentNameMaybe.v].program;
+                console.log('\tSwitched to department %s with program %s', departmentName, programName)
+            }
+
+            var lineOfBusiness = AGENCY_LINE_OF_BUSINESS[departmentName];
+            var characterClass = characterClasses[groupNumber];
+
+            console.log('\tLine of business is %s', lineOfBusiness);
+
+            for (var col = FIRST_FUND_COL; col < FINAL_FUND_COL; col++) {
+                var fundNumber = fundNumbers[col];
+                var fundDescription = fundDescriptions[fundNumber];
+                var opUnitDescription = getOperUnitDesc(fundNumber);
+
+                var opUnitType = getOperatingUnitType(fundNumber);
+                var amount = ExpenseWksht[colIndex(col) + row];
+
+                console.log('For fund number %d, %s...', fundNumber, fundDescription);
+
+                if(amount && amount.v != 0) {
+                    expenses[i++] = {
+                        fundNumber: fundNumber,
+                        fundDescription: fundDescription,
+                        operatingUnitDescription: opUnitDescription,
+                        programName: programName,
+                        lobName: lineOfBusiness,
+                        characterClass: characterClass,
+                        classDescription: characterClass,
+                        accountDescription: departmentName,
+                        operatingUnitType: opUnitType,
+                        amount: amount.v
+                    };
+
+                    console.log('Department %s with fund number %d, operating unit %s, spends %s', departmentName, fundNumber, opUnitDescription, amount.v);
+                } else {
+                    console.log('Col %s is blank.', colIndex(col));
+                }
+            }
+        } else {
+            console.log('Row %d is either a subtotal row or blank', row);
+        }
+    }
+
+    return expenses;
+}
+
 console.log('Opening Tulsa Budget data...');
 
 var workbook = xlsx.readFile(INPUT_SPREADSHEET_LOCATION);
@@ -78,22 +197,23 @@ console.log('Opened file. Finding operating and capital tab...');
 var dollarWksht = workbook.Sheets[AMOUNT_TBL_SHEET_NM];
 var categoryDescWorksheet = workbook.Sheets[CATG_SHEET_NM];
 
-console.log('Finding fund programs...');
-var programs = getPrograms(categoryDescWorksheet);
 
 console.log('Getting fund numbers...');
 var fundNumbers = getFundNumbers(dollarWksht, FIRST_FUND_COL, FINAL_FUND_COL);
 
 console.log('Getting fund descriptions...');
 var fundDescriptions = getFundDescriptions(workbook);
-//
-// console.log('Getting revenue source descriptions...');
-// var revenueDescriptions = getRevenueDetailNodes(revenueWorksheet);
-//
-// console.log('Getting amounts for each fund revenue source...');
-// var revenueFigures = getRevenueAmounts(revenueWorksheet, fundNumbers, fundDescriptions, revenueDescriptions, programs);
-//
-// console.log('Writing output file...');
-// jsonfile.writeFile(OUTPUT_JSON_LOCATION, revenueFigures, function (err) {
-//     console.error(err);
-// });
+
+console.log('Getting operating units...');
+
+console.log('Finding fund programs...');
+var programs = getPrograms(categoryDescWorksheet);
+
+
+console.log('Getting amounts for each fund and category source...');
+var expenseAmounts = getExpenseAmounts(dollarWksht, programs, fundNumbers, fundDescriptions);
+
+console.log('Writing output file...');
+jsonfile.writeFile(OUTPUT_JSON_LOCATION, expenseAmounts, function (err) {
+    console.error(err);
+});
